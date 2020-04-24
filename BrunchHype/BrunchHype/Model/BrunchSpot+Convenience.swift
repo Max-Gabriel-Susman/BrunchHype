5859028785d1c635f57c3ba03bf2baac971aa7f3
Moses <gabe.susman@gmail.com>
2020-04-23 18:52:25 -0600

    woot woot

---
+++ BrunchHype/BrunchHype/Model/BrunchSpot+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  BrunchSpot+Convenience.swift
+//  BrunchHype
+//
+//  Created by Moses on 4/23/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension BrunchSpot {
+    
+    // optionality from data model inspector is overriden by these paramsB
+    convenience init(name: String, tier: String = "Unrated", summary: String = "No summary added", context: NSManagedObjectContext = CoreDataStack.context) {
+        self.init(context: context)
+        self.name = name
+        self.tier = tier
+        self.summary = summary
+    }
+}
